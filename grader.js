30ff44d1af42eb89b05a4891f2c37fc13d2f124c
Ganji <gsamatha@yahoo.com>
2013-07-13 06:02:17 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attributes. Uses commander.js and cheerio. Teaches command line application development and basic DOM parsing.
+Automatically grade files for the presenceof specified HTML tags/attributes. Uses commander.js and cheerio. Teaches command line application development and basic DOM parsing.
 
 References:
 
@@ -22,6 +22,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,6 +46,7 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+     console.log($);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -52,6 +55,28 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+
+
+var loadWithRestler = function(url, checksFile) {
+    rest.get(url).on('complete', function (result){
+      if (result instanceof Error) {
+        console.log('Error: ' + result.message);
+        process.exit(1);
+      } else {
+	  $ = cheerio.load(result);
+	  var checks = loadChecks(checksFile).sort();
+	  var out = {};
+	  for(var ii in checks) {
+              var present = $(checks[ii]).length > 0;
+              out[checks[ii]] = present;
+	  }
+	  var outJson = JSON.stringify(out, null, 4);
+          console.log(outJson);
+      };
+    });
+};
+
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -63,10 +88,16 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'http://mighty-wave-7195.herokuapp.com')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+//	console.log("This will print while url is still being fetched.");
+	loadWithRestler(program.url, program.checks);
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
