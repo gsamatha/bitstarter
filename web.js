4e696887b2c1a0f2788e405afa53c8d8cb370145
gsamatha <gsamatha@yahoo.com>
2013-08-08 06:55:49 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 app.use(express.static(__dirname + '/public'));
-app.listen(8080);
+//app.listen(8080);
 app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html", 'utf8'));
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
